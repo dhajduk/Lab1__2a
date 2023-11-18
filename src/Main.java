8edc34e14c29a6fea0e1e70b435ffbced27a7cf8
dhajduk <gato123@poczta.fm>
2023-11-18 23:28:08 +0100

    Zad.5

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+class CreateGUI {
+    public static void main() {
+        JFrame frame = new JFrame();
+        frame.setSize(640,480);
+        frame.setVisible(true);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        CreateGUI.main();
+    }
+}
